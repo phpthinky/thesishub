dd4e53bbee3a50643bd9e38a3ef9d1b553c3b9b3
Harold Rita <letswrite14@gmail.com>
2018-04-26 03:24:45 +0800

    lock app

--- application/views/excel/import.php
+++
@@ -1,7 +0,0 @@
-						
-<?php echo form_open_multipart('backup/ExcelDataAdd');?>                      
-<label>Excel File:</label>                        
-<input type="file" name="userfile" />				                   
-<input type="submit" value="upload" name="upload" />
-</form>	
-</div
